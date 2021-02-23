c5263582e53972ec2a2aa5a95e98a27b57112000
Yegor Bitensky <yegorbitensky@gmail.com>
2021-02-23 17:45:48 +0300

    Add bubble sort with latest swap index save

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -48,17 +48,49 @@ def bubble_sort_with_need_to_swap_check(items):
 
     n = len(items) # O(1)
     need_to_swap = True
-    for i in range(n - 2, -1, -1): # O((n - 1) / 2)
+    for i in range(n - 2, -1, -1): # O(n - 1)
         next_item_index = n - i - 2
         if need_to_swap:
             need_to_swap = False
             for j in range(n - 1, next_item_index, -1):
                 '''
-                from O(n - 1) (if outer cycle contains 1 iteration)
-                to O(n / 2) (if outer cycle contains n - 1 iteration)
+                from O(n / 2) (if outer cycle contains n - 1 iterations with swaps)
+                to O(n - 1) (if outer cycle contains 1 iteration with swaps)
                 '''
                 if items[j] < items[j - 1]:
                     items[j], items[j - 1] = items[j - 1], items[j]
                     need_to_swap = True
+        yield items[next_item_index]
+    yield items[n - 1]
+
+
+def bubble_sort_with_latest_swap_index_save(items):
+    '''
+    Bubble sort with latest swap index save.
+
+    items - list of comparable items.
+
+    ```python
+    >>> items = [1, 9, 5, 2, 7, 3, 8, 4, 6]
+    >>> sorted_items = bubble_sort_with_latest_swap_index_save(items)
+    >>> sorted_items
+    <generator object bubble_sort_with_latest_swap_index_save at 0x7fc467d3d190>
+    >>> tuple(sorted_items)
+    (1, 2, 3, 4, 5, 6, 7, 8, 9)
+    ```
+
+    Total time cost:
+    ~ O(n^2)
+    '''
+
+    n = len(items) # O(1)
+    latest_swap_index = 0
+    for i in range(n - 2, -1, -1): # O(n - 1)
+        next_item_index = n - i - 2
+        if latest_swap_index < n - 1:
+            for j in range(n - 1, latest_swap_index, -1): # from O(1) to O(n - 1)
+                if items[j] < items[j - 1]:
+                    items[j], items[j - 1] = items[j - 1], items[j]
+                    latest_swap_index = j
         yield items[next_item_index]
     yield items[n - 1]
