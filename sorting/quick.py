a81de7d2a0b59c4ae94799b60b9c5632c731c20c
Yegor Bitensky <yegorbitensky@gmail.com>
2021-08-14 16:14:12 +0300

    Add tail recursive quick sort

--- sorting/quick.py
+++ sorting/quick.py
@@ -58,3 +58,35 @@ def quick_sort(items, l=0, r=None):
     m = quick_sort_partition(items, l, r)
     quick_sort(items, l=l, r=m - 1)
     quick_sort(items, l=m + 1, r=r)
+
+
+def tail_recursive_quick_sort(items, l=0, r=None):
+    '''
+    Tail recursive quick sort.
+
+    items - list of comparable items.
+    l - start index
+    r - end index
+
+    ```python
+    >>> items = [1, 9, 5, 2, 7, 3, 8, 4, 6]
+    >>> tail_recursive_quick_sort(items)
+    >>> items
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ```
+
+    Total time cost:
+    ~ O(n * log(n))
+    '''
+
+    if r is None:
+        r = len(items) - 1
+
+    while l < r:
+        m = quick_sort_partition(items, l, r)
+        if m - l > r - m:
+            tail_recursive_quick_sort(items, l=m + 1, r=r)
+            r = m - 1
+        else:
+            tail_recursive_quick_sort(items, l=l, r=m - 1)
+            l = m + 1
