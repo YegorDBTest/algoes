75bc753b7b867485e05363d9ea6c44de06ac0faa
Yegor Bitensky <yegorbitensky@gmail.com>
2021-09-19 18:32:30 +0300

    Add BinarySearchMin and BinarySearchMax

--- binary_search.py
+++ binary_search.py
@@ -1,48 +1,108 @@
-def binary_search(data, item):
+class BinarySearch:
     '''
     Binary search.
 
-    data - sorted list.
-    item - item to search in data.
+    Get first matched item index.
 
     Average complexity - O(log n).
     '''
 
-    lower_index = 0
-    higher_index = len(data) - 1
-    while higher_index >= lower_index:
-        half_index = lower_index + (higher_index - lower_index) // 2
-        if item == data[half_index]:
-            return half_index
-        elif item > data[half_index]:
-            lower_index = half_index + 1
-        else:
-            higher_index = half_index - 1
-    return -1
+    def __init__(self, data, item):
+        '''
+        data - sorted list.
+        item - item to search in data.
+        '''
+        self._data = data
+        self._item = item
+        self._lower_index = 0
+        self._higher_index = len(data) - 1
+        self.index = self._get_index()
+
+    def _get_index(self):
+        while self._higher_index >= self._lower_index:
+            half_index = self._get_half_index(self._lower_index, self._higher_index)
+            if self._item == self._data[half_index]:
+                return self._handle_equal(half_index)
+            elif self._item > self._data[half_index]:
+                self._handle_lower(half_index)
+            else:
+                self._handle_higher(half_index)
+        return -1
+
+    def _get_half_index(self, lower_index, higher_index):
+        return lower_index + (higher_index - lower_index) // 2
+
+    def _handle_equal(self, index):
+        return index
+
+    def _handle_lower(self, index):
+        self._lower_index = index + 1
+
+    def _handle_higher(self, index):
+        self._higher_index = index - 1
 
 
-def binary_search_nearest(data, item):
+class BinarySearchMin(BinarySearch):
     '''
-    Binary search witch returns data of max lower index and min higher index.
+    Min binary search.
 
-    data - sorted list.
-    item - item to search in data.
+    Find min matching item index if any, -1 otherwise.
+    Additionaly find max index of item lower than matching item if any,
+        -1 otherwise.
 
     Average complexity - O(log n).
     '''
 
-    lower_index = 0
-    higher_index = len(data) - 1
-    max_lower_index = None
-    min_higher_index = None
-    while higher_index >= lower_index:
-        half_index = lower_index + (higher_index - lower_index) // 2
-        if item == data[half_index]:
-            return half_index, None, None
-        elif item > data[half_index]:
-            max_lower_index = half_index
-            lower_index = half_index + 1
-        else:
-            min_higher_index = half_index
-            higher_index = half_index - 1
-    return -1, max_lower_index, min_higher_index
+    def __init__(self, data, item):
+        '''
+        data - sorted list.
+        item - item to search in data.
+        '''
+        self.max_lower_index = -1
+        super().__init__(data, item)
+
+    def _handle_equal(self, index):
+        while index - self.max_lower_index > 1:
+            half_index = self._get_half_index(self.max_lower_index, index)
+            if self._item == self._data[half_index]:
+                index = half_index
+            else:
+                self.max_lower_index = half_index
+        return index
+
+    def _handle_lower(self, index):
+        super()._handle_lower(index)
+        self.max_lower_index = index
+
+
+class BinarySearchMax(BinarySearch):
+    '''
+    Max binary search.
+
+    Find max matching item index if any, -1 otherwise.
+    Additionaly find min index of item higher than matching item if any,
+        len(data) otherwise.
+
+    Average complexity - O(log n).
+    '''
+
+    def __init__(self, data, item):
+        '''
+        data - sorted list.
+        item - item to search in data.
+        '''
+        self.min_higher_index = len(data)
+        super().__init__(data, item)
+
+    def _handle_equal(self, index):
+        while self.min_higher_index - index > 1:
+            half_index = self._get_half_index(index, self.min_higher_index)
+            if self._item == self._data[half_index]:
+                index = half_index
+            else:
+                self.min_higher_index = half_index
+        return index
+
+    def _handle_higher(self, index):
+        super()._handle_higher(index)
+        self.min_higher_index = index
