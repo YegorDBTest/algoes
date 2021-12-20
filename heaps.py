5d733d8b6ff893e38128262c6e4e842c5ec838e1
Yegor Bitensky <yegorbitensky@gmail.com>
2021-12-20 22:55:46 +0300

    Add in place sort heap

--- heaps.py
+++ heaps.py
@@ -58,3 +58,41 @@ class MinHeap(BaseHeap):
 class MaxHeap(BaseHeap):
     MIN = float('-inf')
     gte = operator.ge
+
+
+class InPlaceSortHeap:
+
+    def __init__(self, items):
+        self.count = len(items)
+        self.items = items
+        self._restore()
+        self._sort()
+
+    def _restore(self):
+        for i in range(self.count // 2 - 1, -1, -1):
+            self._sift_down(i)
+
+    def _sort(self):
+        while self.count > 1:
+            self._swap_items(0, self.count - 1)
+            self.count -= 1
+            self._sift_down(0)
+
+    def _sift_down(self, i):
+        while True:
+            l_i = i * 2 + 1
+            r_i = i * 2 + 2
+
+            l_child = self.items[l_i] if l_i < self.count else None
+            r_child = self.items[r_i] if r_i < self.count else None
+
+            if ((not l_child or self.items[i] > l_child)
+                and (not r_child or self.items[i] > r_child)):
+                break
+
+            max_i = l_i if (not r_child or l_child > r_child) else r_i
+            self._swap_items(i, max_i)
+            i = max_i
+
+    def _swap_items(self, i1, i2):
+        self.items[i1], self.items[i2] = self.items[i2], self.items[i1]
